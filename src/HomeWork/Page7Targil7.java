40e2c8bb59ea24e321ad6459a8d4beda069df289
Itamar Huli <huli2000@gmail.com>
2021-06-03 15:23:13 +0300

    adding Targil7

--- src/HomeWork/Page7Targil7.java
+++ src/HomeWork/Page7Targil7.java
@@ -2,6 +2,20 @@ package HomeWork;
 
 public class Page7Targil7 {
 public static void main(String[] args) {
-	
+
+		//int y = 1400;
+		
+		int y = (int) (Math.random() * (2000-950) + 950);
+		
+		if (y % 4 == 0 && y % 100 != 0|| y %400 == 0 ) {
+		
+		System.out.println(y + " Leap year");
+		
+		}
+		else {
+			
+		System.out.println(y + " Not a leap year");
+		}
 }
+
 }
